9f554aaa3b06fd6205a36fe88879df81f5021481
Mukesh16407 <mukesh.16407@gmail.com>
2023-02-23 10:19:17 +0530

    url fix

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:6010"
+//export const BASE_URL = "http://localhost:6010"
 
 //export const BASE_URL="https://mernapp-f80c.onrender.com"
+export const BASE_URL="https://lovely-tights-fly.cyclic.app/"
